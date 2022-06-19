676a63d3d0654ab015289ac92016e94293b549ad
Stanislaw Cruz <stanycruz@outlook.com>
2022-06-19 11:29:40 -0300

    chore: send requests

--- src/config/database.js
+++ src/config/database.js
@@ -6,3 +6,11 @@ module.exports = mongoose.connect('mongodb://localhost/mymoney', {
     "pass": "dfweumro",
     "useMongoClient": true
 })
+
+mongoose.Error.messages.general.required = "O atributo '{PATH}' é obigatório"
+mongoose.Error.messages.Number.min =
+  "O '{VALUE}' informado é menor que o limite mínimo de '{MIN}'."
+mongoose.Error.messages.Number.max =
+  "O '{VALUE}' informado é maior que o limite máximo de '{MAX}'."
+mongoose.Error.messages.Number.enum =
+  "'{VALUE}' não é válido para o atributo '{PATH}'."
